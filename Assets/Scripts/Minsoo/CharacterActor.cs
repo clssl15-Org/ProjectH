f73f66fb0a80c0151e52dd53978179c7cbe09794
minsoo <40525360+clssl15@users.noreply.github.com>
2025-07-25 14:25:21 +0900

    feat: 플레이어 조작 시스템 구현 3차

--- Assets/Scripts/Minsoo/CharacterActor.cs
+++ Assets/Scripts/Minsoo/CharacterActor.cs
@@ -4,5 +4,66 @@ using UnityEngine;
 
 public class CharacterActor : MonoBehaviour
 {
+    public RigidbodyComponent RigidbodyComponent { get; }
+    public Vector3 Velocity
+    {
+        get => RigidbodyComponent.Velocity;
+        set => RigidbodyComponent.Velocity = value;
+    }
 
+    public Vector3 Position
+    {
+        get => RigidbodyComponent.Position;
+        set => RigidbodyComponent.Position = value;
+    }
+    public Quaternion Rotation
+    {
+        get => transform.rotation;
+        set => transform.rotation = value;
+    }
+
+    public bool IsGrounded { get; private set; }
+
+    private void PreSimulationUpdate(float dt)
+    {
+        ProcessVelocity(dt);
+    }
+
+    void ProcessVelocity(float dt)
+    {
+        Vector3 position = Position;
+
+        if (IsGrounded)
+            ProcessStableMovement();
+        else
+            ProcessUnstableMovement();
+
+        Velocity = (position - Position) / dt;
+    }
+
+    void ProcessStableMovement()
+    {
+
+    }
+
+    void ProcessUnstableMovement()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        float dt = Time.deltaTime;
+
+        PreSimulationUpdate(dt);
+        
+        //이 함수로 인해 위치 값이 변경됨. position을 바꿔주는 기능과 input값을 받아들이는 함수는 어디?
+        transform.SetPositionAndRotation(Position, Rotation);
+        //오히려 physicsActor의 PlanarVelocity가 바꿔주는것 같기도
+        // 일단, CharacterStateController - FixedUpdate - movementReferenceParameters에서 @movement값 전달
+        // forward * @movement값이 movementReferenceParameters의 InputMovementReference 변수에 저장되는데
+        // 결국, CharacterStateController가 프로퍼티로 가지고 있으며, 외부에서 이것을 참조함.
+        // zeroGravity의 ProcessVelocity()함수 에서 이 값을 사용하여 CharacterActor.Velocity값을 바꿔줌.
+        // zeroGravity는 CharacterState를 상속받으며, UpdateBehaviour에서 ProcessVelocity()함수를 매프레임 실행함.
+    }
 }
---
+++ Assets/Scripts/Minsoo/RigidbodyComponent.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidbodyComponent : MonoBehaviour
+{
+    public Vector3 Velocity { get; set; }
+    public Vector3 Position { get; set; }
+}
