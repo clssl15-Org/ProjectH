0e0efb5cd77ccd4f0c8328ba0e91ee5f91e3787f
dove <dove_creative@naver.com>
2025-12-28 21:42:11 +0900

    Reapply "Merge branch 'origin/merge-251221' of https://github.com/clssl15-Org/ProjectH into origin/merge-251221"
    This reverts commit b3241f3de0c1e22cf152b982d96dafdb26790444.

--- Assets/Scripts/Actors/Monsters/3. Bosses/DarkTherion/DarkTherion.cs
+++ Assets/Scripts/Actors/Monsters/3. Bosses/DarkTherion/DarkTherion.cs
@@ -142,6 +142,8 @@ namespace Actors.Monsters.Bosses
                 );
                 AddChild(new MonsterAction(MonsterActionType.Dead)
                     .AddAnimationComponent()
+                    .AddComponent(new Do(true)
+                        .OnOpening(() => monster.Rigidbody.gravityScale = 1f))
                 );
             }
         }
