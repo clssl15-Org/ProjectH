df8d7091686ecb0dc638b204b001aaa9e2e475f2
minsoo <40525360+clssl15@users.noreply.github.com>
2026-01-20 03:28:34 +0900

    feat: 기사단의 최후 맹세석 구현

--- Assets/Scripts/Actors/RelicSystem/Relic/KnighthoodLastOathStone.cs
+++ Assets/Scripts/Actors/RelicSystem/Relic/KnighthoodLastOathStone.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
+using Actors.PlayerSystem;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class KnighthoodLastOathStone : MonoBehaviour
+public class KnighthoodLastOathStone : Relic
 {
-    // Start is called before the first frame update
-    void Start()
+    private PlayerHealth playerHealth;
+    private bool isActivated = false;
+    public override void OnAcquire()
     {
-        
-    }
+        OnReinforcedAcquire();
 
-    // Update is called once per frame
-    void Update()
+        playerHealth = RelicManager.Instance.player.PlayerHealth;
+        playerHealth.Damaged += Activate;
+        playerHealth.Healed += Activate;
+    }
+    public override void OnLose()
     {
-        
+        playerHealth.Damaged -= Activate;
+        playerHealth.Healed -= Activate;
+
+        if (isActivated)
+        {
+            isActivated = false;
+            RelicManager.Instance.player.playerStats.attackPowerMultiplier /= 2;
+        }
+    }
+    public void Activate()
+    {
+        if (!isActivated)
+        {
+            if (playerHealth.CurrentHealth <= playerHealth.MaxHealth * value)
+            {
+                isActivated = true;
+                RelicManager.Instance.player.playerStats.attackPowerMultiplier *= 2;
+            }
+        }
+        else if (isActivated)
+        {
+            if (playerHealth.CurrentHealth > playerHealth.MaxHealth * value)
+            {
+                isActivated = false;
+                RelicManager.Instance.player.playerStats.attackPowerMultiplier /= 2;
+            }
+        }
     }
 }
