b1a28700683192849d3134082f2f4ec3971ef143
minsoo <clssl15@naver.com>
2026-02-09 17:13:20 +0900

    chore: 패럴랙스 배경 자연스럽게 수정

--- Assets/Scripts/Game/Background/ParallaxCamera.cs
+++ Assets/Scripts/Game/Background/ParallaxCamera.cs
@@ -13,7 +13,7 @@ public class ParallaxCamera : MonoBehaviour
         oldPosition = transform.position.x;
     }
 
-    void Update()
+    void LateUpdate()
     {
         if (transform.position.x != oldPosition)
         {
