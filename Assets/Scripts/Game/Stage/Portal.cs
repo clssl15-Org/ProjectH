a8efbf9e1f90e42f39b06a34616c3a4d0293a16f
minsoo <40525360+clssl15@users.noreply.github.com>
2026-03-05 02:43:34 +0900

    feat: 포탈 생성 효과 추가

--- Assets/Scripts/Game/Stage/Portal.cs
+++ Assets/Scripts/Game/Stage/Portal.cs
@@ -8,12 +8,18 @@ public class Portal : MonoBehaviour
     [SerializeField]
     private GameObject Fsprite;
     [SerializeField]
+    private GameObject mask;
+    [SerializeField]
     private bool stageChange;
 
     private bool isPlayerInRange = false;
     private void Start()
     {
         Fsprite.SetActive(false);
+    }
+    private void OnEnable()
+    {
+        StartCoroutine(MoveMaskUp());
     }
     private void Update()
     {
@@ -47,5 +53,21 @@ public class Portal : MonoBehaviour
 
         isPlayerInRange = false;
         Fsprite.SetActive(false);
+    }
+    IEnumerator MoveMaskUp()
+    {
+        Vector3 startPosition = mask.transform.position;
+        Vector3 endPosition = startPosition + new Vector3(0, 3.5f, 0);
+        float elapsedTime = 0f;
+
+        while (elapsedTime < 2)
+        {
+            mask.transform.position = Vector3.Lerp(startPosition, endPosition, elapsedTime / 2);
+
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        mask.transform.position = endPosition;
     }
 }
