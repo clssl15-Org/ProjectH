ad6dbb69470f636af9ee98e361420f449e8bc943
dove <dove_creative@naver.com>
2026-06-17 22:39:01 +0900

    remove blackbox

--- Assets/Scripts/Infrastructure/Blackbox/Tests/Legacy/NamedOwner.cs
+++
@@ -1,9 +0,0 @@
-namespace BlackboxSystem.Tests
-{
-    internal class NamedOwner
-    {
-        public string Name { get; }
-        public NamedOwner(string name) => Name = name;
-        public override string ToString() => Name;
-    }
-}
