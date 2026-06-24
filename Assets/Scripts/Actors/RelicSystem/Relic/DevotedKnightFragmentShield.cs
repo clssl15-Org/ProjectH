7876b9d4b6531cb613f26758e5535e4dfaa9dc56
dove <dove_creative@naver.com>
2026-06-25 02:26:52 +0900

    Persist player shield count and rebind fragment shield

--- Assets/Scripts/Actors/RelicSystem/Relic/DevotedKnightFragmentShield.cs
+++ Assets/Scripts/Actors/RelicSystem/Relic/DevotedKnightFragmentShield.cs
@@ -1,17 +1,63 @@
-using System.Collections;
-using System.Collections.Generic;
+using Actors.PlayerSystem;
 using UnityEngine;
 
-public class DevotedKnightFragmentShield : Relic
+public class DevotedKnightFragmentShield : Relic, IRelicPlayerRebindHandler
 {
+    private PlayerHealth subscribedPlayerHealth;
+
     public override void OnAcquire()
     {
-        RelicManager.Instance.player.sieldCount += (int)value;
-        RelicManager.Instance.player.PlayerHealth.OnSieldBreak += OnLose;
+        var player = GetPlayer();
+        player.sieldCount += Mathf.RoundToInt(value);
+        SubscribeToShieldBreak(player);
+    }
+
+    public void RebindPlayer(Player player)
+    {
+        SubscribeToShieldBreak(player);
     }
 
     protected override void OnLoseCore()
     {
-        RelicManager.Instance.player.PlayerHealth.OnSieldBreak -= OnLose;
+        UnsubscribeFromShieldBreak();
+    }
+
+    private void SubscribeToShieldBreak(Player player)
+    {
+        if (player == null)
+            throw new System.ArgumentNullException(nameof(player));
+
+        var playerHealth = player.PlayerHealth;
+        if (playerHealth == null)
+            throw new System.InvalidOperationException("[DevotedKnightFragmentShield] 플레이어의 PlayerHealth를 찾을 수 없습니다.");
+
+        if (subscribedPlayerHealth == playerHealth)
+            return;
+
+        UnsubscribeFromShieldBreak();
+        subscribedPlayerHealth = playerHealth;
+        subscribedPlayerHealth.OnSieldBreak += OnLose;
+    }
+
+    private void UnsubscribeFromShieldBreak()
+    {
+        if (subscribedPlayerHealth != null)
+            subscribedPlayerHealth.OnSieldBreak -= OnLose;
+
+        subscribedPlayerHealth = null;
+    }
+
+    private static Player GetPlayer()
+    {
+        var player = RelicManager.Instance?.player ?? FindObjectOfType<Player>();
+        if (player == null)
+            throw new System.InvalidOperationException("[DevotedKnightFragmentShield] 플레이어를 찾을 수 없습니다.");
+
+        return player;
+    }
+
+    private void OnDestroy()
+    {
+        UnsubscribeFromShieldBreak();
     }
 }
